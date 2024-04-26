4692b6be190ff2a7ed4e254ab4571b20b453fb61
Aleksandar <aleksandar@e-vlasotince.info>
2024-04-26 19:29:50 +0200

    fetch data on server

--- app/(content)/news/page.js
+++ app/(content)/news/page.js
@@ -1,11 +1,17 @@
-import { DUMMY_NEWS } from "@/dummy-news";
+// import { DUMMY_NEWS } from "@/dummy-news";
 import NewsList from "@/components/NewsList";
 
-const NewsPage = () => {
+const NewsPage = async () => {
+  const response = await fetch("http://localhost:8080/news");
+  if (!response.ok) {
+    throw new Error("News cant be fetched");
+  }
+  const news = await response.json();
+
   return (
     <>
       <h1>News Page</h1>
-      <NewsList news={DUMMY_NEWS} />
+      <NewsList news={news} />
     </>
   );
 };
